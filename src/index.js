d62ae90f248c2abbe3578799b0e260423b481905
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-09-19 20:58:19 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  let j = 1,
+      i = 2,
+      a = [],
+      copyNumber = number;
+
+  do {
+    if (base % i === 0) {
+      a[j] = i;
+      j++;
+      base = base / i;
+    } else {
+      i++;
+    }
+  } while (i < base);
+  a[j] = i;
+  let res = {};
+  a.forEach(function (e) {
+    res[e] = 1 + ~~res[e];
+  });
+
+  function sortObject(obj) {
+    let arr = [];
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        arr.push({
+          'base': prop,
+          'degree': obj[prop]
+        });
+      }
+    }
+    return arr; 
+  }
+  let arr = sortObject(res),
+      degreeOf = arr[arr.length - 1],
+      zerosCount = 0,
+      power = 1,
+      denominator = 0;
+  while (copyNumber > denominator) {
+    denominator = Math.pow(+degreeOf.base, power);
+    zerosCount += Math.floor(number / denominator);
+    power++;
+  }
+  zerosCount /= degreeOf.degree;
+  return Math.trunc(zerosCount);
 }
